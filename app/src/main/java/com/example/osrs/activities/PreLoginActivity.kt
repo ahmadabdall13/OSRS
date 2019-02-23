1cef1a0b5164a924073655bc61f210c95924a8a7
ahmadabdallah13 <a7mtoo133@gmail.com>
2019-02-23 17:48:00 +0200

    wf4grgf4

--- app/src/main/java/com/example/osrs/activities/PreLoginActivity.kt
+++ app/src/main/java/com/example/osrs/activities/PreLoginActivity.kt
@@ -6,45 +6,16 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.example.osrs.R
 import com.example.osrs.activities.fragments.MyPagerAdapter
-import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_pre_login.*
 import kotlinx.android.synthetic.main.fragment_products.*
 
 
-class PreLoginActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class PreLoginActivity : AppCompatActivity() {
 
-    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
 
-        when (menuItem.itemId) {
-            R.id.account -> {
-                Toast.makeText(this, "Account bitch", Toast.LENGTH_SHORT).show()
-            }
-            R.id.settings -> {
-                Toast.makeText(this, "Fuck Your Settings", Toast.LENGTH_SHORT).show()
-            }
-            R.id.mycart -> {
-                Toast.makeText(this, "And Fuck Your Cart", Toast.LENGTH_SHORT).show()
-            }
 
-        }
-        navigationDrawer.closeDrawer(GravityCompat.START)
-
-        return true
-    }
-    override fun onBackPressed() {
-        if (navigationDrawer.isDrawerOpen(GravityCompat.START)) {
-            navigationDrawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    companion object {
-    fun newInstance(): PreLoginActivity = PreLoginActivity()
-}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
