b6e393c22316b2da762e0cbef3bbf933f945a6f1
ahmadabdallah13 <a7mtoo133@gmail.com>
2019-03-05 07:34:40 +0200

    update

--- app/src/main/java/com/example/osrs/activities/PreLoginActivity.kt
+++ app/src/main/java/com/example/osrs/activities/PreLoginActivity.kt
@@ -1,11 +1,17 @@
 package com.example.osrs.activities
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import com.example.osrs.AddProductActivity
 import com.example.osrs.R
 import com.example.osrs.activities.fragments.MyPagerAdapter
 import kotlinx.android.synthetic.main.activity_pre_login.*
@@ -28,10 +34,61 @@ class PreLoginActivity : AppCompatActivity() {
         tabs_main.setupWithViewPager(viewpager_main)
 
 
+        // Configure action bar
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.title = "Fuck Zaid and his gf"
 
+
+        // Initialize the action bar drawer toggle instance
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ){
+            override fun onDrawerClosed(view: View){
+                super.onDrawerClosed(view)
+                //toast("Drawer closed")
+            }
+
+            override fun onDrawerOpened(drawerView: View){
+                super.onDrawerOpened(drawerView)
+                //toast("Drawer opened")
+            }
+
+
+        }
+
+
+        // Configure the drawer layout to add listener and show icon on toolbar
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+
+        // Set navigation view navigation item selected listener
+        navigation_view.setNavigationItemSelectedListener{
+
+                // Handle navigation view item clicks here.
+                when (it.itemId) {
+                    R.id.addProduct -> {
+                        val intent = Intent(applicationContext, AddProductActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                }
+            // Close the drawer
+            drawer_layout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
 
 
+    // Extension function to show toast message easily
+    private fun Context.toast(message:String){
+        Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
+    }
 }
-
