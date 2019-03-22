14df454accf2195ddede16ab3caa1d367006ad1a
zaidsallam <z.sallam@aounservices.com>
2019-03-22 22:19:44 +0200

    fix all bugs and facebook and add product done

--- app/src/main/java/com/example/osrs/services/APIController.kt
+++ app/src/main/java/com/example/osrs/services/APIController.kt
@@ -5,11 +5,17 @@ import android.content.Context
 
 class APIController constructor(serviceInjection: ServiceInterface): ServiceInterface {
 
+
+
     private val service: ServiceInterface = serviceInjection
+
+
+
 
     override fun login(Email:String,Password:String,context: Context) {
         service.login(Email,Password,context)
     } // end fun login
+
 
     override fun singUp(
         Email: String,
@@ -18,9 +24,36 @@ class APIController constructor(serviceInjection: ServiceInterface): ServiceInte
         LastName: String,
         MobileNumber: String,
         UserType:Int,
+        socialId:String,
         context: Context
     ) {
-        service.singUp(Email,Password,FirstName,LastName, MobileNumber,UserType,context)
+        service.singUp(Email,Password,FirstName,LastName, MobileNumber,UserType,socialId,context)
     } // end singUp
+
+
+
+    override fun loginFacebook(
+        socialId: String,
+        context: Context
+    ) {
+        service.loginFacebook(socialId,context)
+    } // end singUp
+
+
+
+
+
+    override fun addProduct(
+        brandName:String,modelName:String,yearOfMake:String,
+        typeOfEngine:String,typeOfTransmission:String,price:Double,mileage:String,externalColor:String,
+        internalColor:String, description:String,
+        context: Context
+    ) {
+        service.addProduct(brandName,modelName,yearOfMake,typeOfEngine,
+            typeOfTransmission,price,mileage,externalColor,internalColor,description,context)
+    } // end singUp
+
+
+
 
 } // end APIController
--- app/src/main/java/com/example/osrs/services/ServiceVolley.kt
+++ app/src/main/java/com/example/osrs/services/ServiceVolley.kt
@@ -24,9 +24,9 @@ class ServiceVolley : ServiceInterface {
 
 
     val TAG = ServiceVolley::class.java.simpleName
-    val basePath = "http://18.219.85.157:5000/"
+    val basePath = "http://18.219.85.157/"
 
-    override fun login(Email:String,Password:String,context: Context) {
+    override fun login(Email: String, Password: String, context: Context) {
 
         val loginJO = JSONObject()
         loginJO.put("email_address", Email)
@@ -37,32 +37,32 @@ class ServiceVolley : ServiceInterface {
             Request.Method.POST, basePath + "login", loginJO,
             Response.Listener<JSONObject> { response ->
 
-                var id =0
+                var id = 0
 
-                if (response.has("id")){
-                     id = response["id"].toString().toInt()
+                if (response.has("id")) {
+                    id = response["id"].toString().toInt()
                 } // end if
                 else {
-                    Toast.makeText(context,"Email Or Password Incorrect , Try Again", Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, "Email Or Password Incorrect , Try Again", Toast.LENGTH_LONG).show()
                     return@Listener
                 } // end else
 
 
 //                if ( id > 0){
-                    val Prefs = Prefs(context)
+                val Prefs = Prefs(context)
 
-                    Prefs.userId = response["id"].toString()
-                    Prefs.firstName = response["first_name"].toString()
-                    Prefs.lastName = response["last_name"].toString()
+                Prefs.userId = response["id"].toString()
+                Prefs.firstName = response["first_name"].toString()
+                Prefs.lastName = response["last_name"].toString()
 
-                    val userId = Prefs.userId
-                    val firstName = Prefs.firstName
-                    val lastName = Prefs.lastName
+                val userId = Prefs.userId
+                val firstName = Prefs.firstName
+                val lastName = Prefs.lastName
 
-                    Toast.makeText(context,"===> $firstName $lastName $userId", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "===> $firstName $lastName $userId", Toast.LENGTH_LONG).show()
 
-                    val intent = Intent(context, PreLoginActivity::class.java)
-                    context.startActivity(intent)
+                val intent = Intent(context, PreLoginActivity::class.java)
+                context.startActivity(intent)
 
 //                }
 //                else{
@@ -73,7 +73,7 @@ class ServiceVolley : ServiceInterface {
             },
             Response.ErrorListener { error ->
                 VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
-                Toast.makeText(context,"Error : Sing The Fuck Up ${error.message}", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Error : Sing The Fuck Up ${error.message}", Toast.LENGTH_LONG).show()
 
 
             }) {
@@ -94,7 +94,8 @@ class ServiceVolley : ServiceInterface {
         FirstName: String,
         LastName: String,
         MobileNumber: String,
-        UserType:Int,
+        UserType: Int,
+        socialId:String,
         context: Context
     ) {
         val signUpJO = JSONObject()
@@ -104,21 +105,24 @@ class ServiceVolley : ServiceInterface {
         signUpJO.put("last_name", LastName)
         signUpJO.put("mobile_number", MobileNumber)
         signUpJO.put("user_type_id", UserType)
+        signUpJO.put("social_id", socialId)
 
 
         val jsonObjReq = object : JsonObjectRequest(
             Request.Method.POST, basePath + "signup", signUpJO,
             Response.Listener<JSONObject> { response ->
 
-                    Toast.makeText(context,"Registration Completed Successfully, Welcome ${response["first_name"]} "
-                        , Toast.LENGTH_LONG).show()
-                    val intent = Intent(context, PreLoginActivity::class.java)
-                    context.startActivity(intent)
+                Toast.makeText(
+                    context, "Registration Completed Successfully, Welcome ${response["first_name"]} "
+                    , Toast.LENGTH_LONG
+                ).show()
+                val intent = Intent(context, PreLoginActivity::class.java)
+                context.startActivity(intent)
 
             },
             Response.ErrorListener { error ->
                 VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
-                Toast.makeText(context,"Error : Sing The Fuck Up ${error.message}", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Error : Sing The Fuck Up ${error.message}", Toast.LENGTH_LONG).show()
 
 
             }) {
@@ -134,4 +138,100 @@ class ServiceVolley : ServiceInterface {
     } // end singUp
 
 
-} // end ServiceVolley
+
+        override fun loginFacebook(socialId: String, context: Context) {
+
+        val loginJO = JSONObject()
+        loginJO.put("social_id", socialId)
+
+
+        val jsonObjReq = object : JsonObjectRequest(
+            Request.Method.POST, basePath + "loginFacebook", loginJO,
+            Response.Listener<JSONObject> { response ->
+
+                if (response.has("id")) {
+                    Toast.makeText(context, "hahah congrats! {$response}", Toast.LENGTH_LONG).show()
+
+                } else {
+                    Toast.makeText(context, "Please Sign The Fuck Up", Toast.LENGTH_LONG).show()
+//                    val intent = Intent(context, SignUpActivity::class.java)
+//                    intent.putExtra("social_id", socialId)
+//                    context.startActivity(intent)
+
+                    val intent = Intent(context, SignUpActivity::class.java)
+                    intent.putExtra("social_id", socialId)
+
+                    context.startActivity(intent)
+
+                }
+
+            },
+            Response.ErrorListener { error ->
+                VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
+                Toast.makeText(context, "Error : Sing The Fuck Up ${error.message}", Toast.LENGTH_LONG).show()
+
+
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                return headers
+            }
+        }
+
+        BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
+    }
+
+
+
+
+
+    override fun addProduct(
+        brandName:String,modelName:String,yearOfMake:String,
+        typeOfEngine:String,typeOfTransmission:String,price:Double,mileage:String,externalColor:String,
+        internalColor:String, description:String,
+        context: Context
+    ) {
+        val productOj = JSONObject()
+        productOj.put("brand_name", brandName)
+        productOj.put("model_name", modelName)
+        productOj.put("year_of_make", yearOfMake)
+        productOj.put("type_of_engine", typeOfEngine)
+        productOj.put("price", price)
+        productOj.put("mileage", mileage)
+        productOj.put("external_color", externalColor)
+        productOj.put("internal_color", internalColor)
+        productOj.put("description", description)
+        productOj.put("product_type_id", 3)
+        productOj.put("vendor_id", 17)
+
+
+
+        val jsonObjReq = object : JsonObjectRequest(
+            Request.Method.POST, basePath + "products", productOj,
+            Response.Listener<JSONObject> { response ->
+
+                Toast.makeText(
+                    context, ", Welcome ${response} "
+                    , Toast.LENGTH_LONG
+                ).show()
+            },
+            Response.ErrorListener { error ->
+                VolleyLog.e(TAG, "/post request fail! Error: ${error.message}")
+                Toast.makeText(context, "Error : Sing The Fuck Up ${error.message}", Toast.LENGTH_LONG).show()
+
+
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                return headers
+            }
+        }
+
+        BackendVolley.instance?.addToRequestQueue(jsonObjReq, TAG)
+    } // end singUp
+
+}
